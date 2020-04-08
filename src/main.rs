df45e5cfeba9a5c07d0f30ae9d76bdd7e2df1ee1
16Yongjin <yongjin0802@gmail.com>
2020-04-08 21:27:24 +0900

    vga_buffer println!

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,14 @@ mod vga_buffer;
 /// This function is called on panic.
 #[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    vga_buffer::Writer::print_something();
+    println!("Hello world{}", "!");
 
     loop {}
 }
